f8ed15d135026e777c6022a8200bdf874b456dea
jwshin <dev.jwshin@gmail.com>
2022-08-05 09:49:00 +0900

    test

--- code/lib/main.dart
+++ code/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MaterialApp(
+      home: MyApp(),
+    )
+  );
 }
 
 class MyApp extends StatelessWidget {
